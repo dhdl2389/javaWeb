4250640797ab51581630c36f2a6f3fa016e68fa9
dhdl2389 <dhdl2389@naver.com>
2024-05-09 16:33:10 +0900

    20240509

--- codetest/src/main/java/codetestFor/유지보수비용2.java
+++ codetest/src/main/java/codetestFor/유지보수비용2.java
@@ -1,30 +1,32 @@
 package codetestFor;
 
+import java.util.Scanner;
+
 public class 유지보수비용2 {
 	public static void main(String[] args) {
 // 문제: 문제가 A 업체는 한달의 유지보수 비용이 10만원이 든다.
 //		B업체는 한달의 유지보수 비용이 20만원이 든다.
 //		A업체는 24년 3월 부터 사용했고 B업체는 23년 1월부터 사용했다. 24년 유지보수 비용과 23년 유지보수 전체 비용을 구하여라 
 
-		int year = 24;
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+
+		int aCost = 100000;
+		int bCost = 200000;
 
 		int aStartMonth = 3;
 		int bStartMonth = 1;
+		int result;
 
-		int aMonthCost = 100000;
-		int bMonthCost = 200000;
-
-		int aTotalCost = 0;
-		int bTotalCost = 0;
-
-		if (year == 24) {
-			aTotalCost += (12 - aStartMonth + 1) * aMonthCost;
-			bTotalCost += (24 - bStartMonth + 1) * bMonthCost;
-			int sum24 = aTotalCost + bTotalCost;			
-			System.out.println(sum24);
+		if (year == 23) {
+			result = (12 - bStartMonth + 1) * bCost;
+			System.out.println(result);
+		} else if (year == 24) {
+            int aCostResult = (12- aStartMonth +1) * aCost;
+            int bCostResult = ((12-bStartMonth + 1) * bCost) + (12 * bCost);
+            System.out.println(aCostResult + bCostResult);
 		}else {
-			bTotalCost +=(12- bStartMonth +1) * bMonthCost;
-			System.out.println(bTotalCost);
+			System.out.println("잘못된 입력");
 		}
 
 	}
--- codetest/src/main/java/codetestFor/임금비용3.java
+++ codetest/src/main/java/codetestFor/임금비용3.java
@@ -8,28 +8,30 @@ public class 임금비용3 {
 		// 회사 X는 2023년 5월부터 서비스를 이용하기 시작했고, 회사 Y는 2022년 10월부터 서비스를 이용하기 시작했습니다.
 		// 2023년과 2022년의 서비스 이용에 대한 인건비를 각각 계산하세요.
 		
+		Integer year = 23;
+		
 		int xCost = 150000;
 		int yCost = 250000;
 		
-		int xStartYear = 23;
-		int yStartYear = 22;
-		
 		int xStartMonth = 5;
 		int yStartMonth = 10;
+				
+		int xCostTotal;
+		int yCostTotal;
 		
-		int xTotalCost=0;
-		int yTotalCost = 0;
 		
-		if(xStartYear == 23) {
-			xTotalCost += (12 -xStartMonth +1) * xCost;			
+		if(year.equals(25)) {
+			yCostTotal = (12- yStartMonth + 1) * yCost;  // 75만
+			System.out.println(yCostTotal);
+		}else if(year ==23){
+			xCostTotal = (12 - xStartMonth + 1) * xCost;   // 120만
+			yCostTotal = ((12 - yStartMonth +1) * yCost) + (yCost * 12); // 75 + 300 = 375만
+			int year23Sum = xCostTotal + yCostTotal;
+			System.out.println(year23Sum);		
+		}else {
+			System.out.println("잘못된 연도");
 		}
-		
-		if(yStartYear ==22) {
-			yTotalCost += (12-yStartMonth +1) * yCost;
-		}
-		
-		System.out.println("22년 인건비는" + " " + yTotalCost);
-		System.out.println("23년 인건비는"+ " " + (xTotalCost + yTotalCost));
+			
 	}
 	
 }
