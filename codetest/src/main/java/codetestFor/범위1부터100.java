c2c63d0a5a5aff7531065173e954120b688d5772
dhdl2389 <dhdl2389@naver.com>
2024-05-10 12:05:23 +0900

    20240510

--- codetest/src/main/java/codetestFor/범위1부터100.java
+++ codetest/src/main/java/codetestFor/범위1부터100.java
@@ -1,13 +1,13 @@
 package codetestFor;
 
 public class 범위1부터100 {
-	
+
 	public static void main(String[] args) {
-		
-		for(int i = 1; i<=100; i++) {	
-			System.out.print(i + " ");
+
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
 		}
-		
+
 	}
-	
+
 }
--- codetest/src/main/java/codetestFor/유지보수비용2.java
+++ codetest/src/main/java/codetestFor/유지보수비용2.java
@@ -8,25 +8,25 @@ public class 유지보수비용2 {
 //		B업체는 한달의 유지보수 비용이 20만원이 든다.
 //		A업체는 24년 3월 부터 사용했고 B업체는 23년 1월부터 사용했다. 24년 유지보수 비용과 23년 유지보수 전체 비용을 구하여라 
 
-		Scanner sc = new Scanner(System.in);
-		int year = sc.nextInt();
+		int year = 24;
 
 		int aCost = 100000;
 		int bCost = 200000;
 
 		int aStartMonth = 3;
 		int bStartMonth = 1;
-		int result;
 
 		if (year == 23) {
-			result = (12 - bStartMonth + 1) * bCost;
-			System.out.println(result);
+
+			int year23 = (12 - bStartMonth + 1) * bCost;
+			System.out.println(year23);
+
 		} else if (year == 24) {
-            int aCostResult = (12- aStartMonth +1) * aCost;
-            int bCostResult = ((12-bStartMonth + 1) * bCost) + (12 * bCost);
-            System.out.println(aCostResult + bCostResult);
+
+			int year24 = (((12 - bStartMonth + 1) * bCost) * 2) + ((12 - aStartMonth + 1) * aCost);
+			System.out.println(year24);
 		}else {
-			System.out.println("잘못된 입력");
+			System.out.println("잘못된 연도");
 		}
 
 	}
